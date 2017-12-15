40c90136570ab26efb7213201ce70a5b33d5c094
ymlu <ymlu@kloudless.com>
2017-12-15 11:42:38 +0800

    Bumping version

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from setuptools import find_packages
 
 
 setup(name='py_lru_cache',
-      version='0.1.4',
+      version='0.1.5a',
       description="""LRU cache for python. Provides a dictionary-like object as well as a method decorator.""",
       author='Chris Stucchio',
       author_email='stucchio@gmail.com',
